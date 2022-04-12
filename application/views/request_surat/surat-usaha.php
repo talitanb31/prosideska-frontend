668c9a00b63bd2623fe7cdf825dbc254113bee12
oong <37205723+oong26@users.noreply.github.com>
2022-04-13 01:26:19 +0700

    Pengajuan surat usaha.

---
+++ application/views/request_surat/surat-usaha.php
@@ -0,0 +1,6 @@
+$(".inputSurat").append(`
+<div class="col-md-12">
+	<label for="">Bergerak dibidang</label>
+	<input type="text" class="form-control" placeholder="ex : Material" name="bidang_usaha">
+</div>
+`)
