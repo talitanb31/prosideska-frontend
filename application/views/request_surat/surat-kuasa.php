b92b9daf2ebbf9cb555bf33c16249079f9233974
oong <37205723+oong26@users.noreply.github.com>
2022-04-13 16:26:09 +0700

    Pengajuan surat kuasa.

---
+++ application/views/request_surat/surat-kuasa.php
@@ -0,0 +1,86 @@
+$(".inputSurat").append(`
+<!-- Yang bertanda tangan -->
+<h5 class="mt-4">Yang bertanda tangan dibawah ini</h5>
+<div class="col-md-6 mb-2">
+	<label for="">NIK</label>
+	<input type="text" class="form-control" placeholder="NIK" name="nik_ttd">
+</div>
+<div class="col-md-6 mb-2">
+	<label for="">Nama Lengkap</label>
+	<input type="text" class="form-control" placeholder="Nama lengkap" name="nama_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Tempat lahir</label>
+	<input type="text" class="form-control" placeholder="Tempat lahir" name="tempat_lahir_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Tanggal lahir</label>
+	<input type="date" class="form-control" placeholder="Taggal lahir" name="taggal_lahir_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Pekerjaan</label>
+	<input type="text" class="form-control" placeholder="Pekerjaan" name="pekerjaan_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Kabupaten</label>
+	<input type="text" class="form-control" placeholder="Kabupaten" name="kab_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Kecamatan</label>
+	<input type="text" class="form-control" placeholder="Kecamatan" name="kecamatan_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Desa</label>
+	<input type="text" class="form-control" placeholder="Desa/Keluarahan" name="desa_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">RT</label>
+	<input type="number" class="form-control" placeholder="RT" name="rt_ttd">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">RW</label>
+	<input type="number" class="form-control" placeholder="RW" name="rw_ttd">
+</div>
+<!-- Penerima kuasa -->
+<h5 class="mt-4">Penerima Kuasa</h5>
+<div class="col-md-6 mb-2">
+	<label for="">NIK</label>
+	<input type="text" class="form-control" placeholder="NIK" name="nik_penerima">
+</div>
+<div class="col-md-6 mb-2">
+	<label for="">Nama Lengkap</label>
+	<input type="text" class="form-control" placeholder="Nama lengkap" name="nama_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Tempat lahir</label>
+	<input type="text" class="form-control" placeholder="Tempat lahir" name="tempat_lahir_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Tanggal lahir</label>
+	<input type="date" class="form-control" placeholder="Taggal lahir" name="taggal_lahir_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Pekerjaan</label>
+	<input type="text" class="form-control" placeholder="Pekerjaan" name="pekerjaan_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Kabupaten</label>
+	<input type="text" class="form-control" placeholder="Kabupaten" name="kab_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Kecamatan</label>
+	<input type="text" class="form-control" placeholder="Kecamatan" name="kecamatan_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">Desa</label>
+	<input type="text" class="form-control" placeholder="Desa/Keluarahan" name="desa_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">RT</label>
+	<input type="number" class="form-control" placeholder="RT" name="rt_penerima">
+</div>
+<div class="col-md-3 mb-2">
+	<label for="">RW</label>
+	<input type="number" class="form-control" placeholder="RW" name="rw_penerima">
+</div>
+`)
