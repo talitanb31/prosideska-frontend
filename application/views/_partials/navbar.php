fd32a91daa78d476838466803d8f2bbfeba4a5ec
Rifjan Jundila <rjndla@gmail.com>
2022-06-22 22:47:48 +0700

    feat: gambar

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -1,7 +1,8 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
 	<a href="index.html" class="navbar-brand d-flex align-items-center text-center py-0 px-4 px-lg-5">
-		<h1 class="m-0 text-primary">WebSurat</h1>
+		<img src="<?='http://localhost/prosideska-frontend/assets/img/logo.png'?>" width="100" height="30" class="d-inline-block align-top">
+		<!-- <h1 class="m-0 text-primary">WebSurat</h1> -->
 	</a>
 	<button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
 		<span class="navbar-toggler-icon"></span>
