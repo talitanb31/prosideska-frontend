cabc79d70f3a523c27f4805cd307c9dee5c5e74f
oong26 <mkhalil26122000@gmail.com>
2022-06-24 12:21:32 +0700

    Benerin footer.

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -3,14 +3,15 @@
 		<div class="row g-5">
 			<div class="col-lg-4 col-md-6">
 				<h5 class="text-white mb-4">Portal Berita</h5>
-				<a class="btn btn-link text-white-50" href="">Alur & Syarat</a>
-				<a class="btn btn-link text-white-50" href="">Request Surat</a>
-				<a class="btn btn-link text-white-50" href="">Notifikasi</a>
+				<a class="btn btn-link text-white-50" href="<?= base_url('berita') ?>">Portal Berita</a>
+				<a class="btn btn-link text-white-50" href="<?= base_url('alursyarat') ?>">Alur & Syarat</a>
+				<a class="btn btn-link text-white-50" href="<?= base_url('requestsurat') ?>">Permintaan Surat</a>
+				<a class="btn btn-link text-white-50" href="<?= base_url('notifikasi') ?>">Notifikasi</a>
 			</div>
-	
+
 			<div class="col-lg-4 col-md-6">
 				<h5 class="text-white mb-4">Contact</h5>
-				<p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
+				<p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Jl. Kunthi, Sukowinangun, Kec. Magetan, Kabupaten Magetan, Jawa Timur 63319</p>
 				<p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
 				<p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
 				<div class="d-flex pt-2">
@@ -20,34 +21,34 @@
 					<a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
 				</div>
 			</div>
-			<div class="col-lg-4 col-md-6">
+			<!-- <div class="col-lg-4 col-md-6">
 				<h5 class="text-white mb-4">Newsletter</h5>
 				<p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
 				<div class="position-relative mx-auto" style="max-width: 400px;">
 					<input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
 					<button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
 				</div>
-			</div>
+			</div> -->
 		</div>
 	</div>
 	<div class="container">
 		<div class="copyright">
 			<div class="row">
 				<div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-					&copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved. 
-					
+					&copy; <a class="border-bottom" href="#">Prosideska</a>, All Right Reserved.
+
 					<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
 					Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
 					</br>Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
 				</div>
-				<div class="col-md-6 text-center text-md-end">
+				<!-- <div class="col-md-6 text-center text-md-end">
 					<div class="footer-menu">
 						<a href="">Home</a>
 						<a href="">Cookies</a>
 						<a href="">Help</a>
 						<a href="">FQAs</a>
 					</div>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</div>
--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-	<a href="index.html" class="navbar-brand d-flex align-items-center text-center py-0 px-4 px-lg-5">
-		<img src="<?='http://localhost/prosideska-frontend/assets/img/logo.png'?>" width="100" height="30" class="d-inline-block align-top">
+	<a href="<?= base_url('') ?>" class="navbar-brand d-flex align-items-center text-center py-0 px-4 px-lg-5">
+		<img src="<?= 'http://localhost/prosideska-frontend/assets/img/logo.png' ?>" width="100" height="30" class="d-inline-block align-top">
 		<!-- <h1 class="m-0 text-primary">WebSurat</h1> -->
 	</a>
 	<button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
